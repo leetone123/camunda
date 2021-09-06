6ce52dd1b57dd4db2170e9af27c1698f1d83ebc7
Tan Lei <leetone123@163.com>
2021-09-06 16:35:00 +0800

    在流程中调用 JAVA Class

---
+++ src/main/java/com/leetone/loanapproval/service/OfferLoanService.java
@@ -0,0 +1,22 @@
+package com.leetone.loanapproval.service;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @Description 发放贷款
+ * @ClassName OfferLoan.java
+ * @Author leetone
+ * @Version 1.0.0
+ * @CreateTime 2021年09月06日 16:05:00
+ */
+public class OfferLoanService implements JavaDelegate {
+  private final static Logger LOGGER = LoggerFactory.getLogger("LOAN-REQUESTS");
+
+  @Override
+  public void execute(DelegateExecution execution) throws Exception {
+    LOGGER.info("offer " + execution.getVariable("lenderId") + " loans");
+  }
+}
